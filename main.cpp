82dbc4c956bd6ddfa66018d62de0f23cb474ed87
dev-groz <groza4ka@gmail.com>
2025-02-17 23:13:50 +0700

    translated comments

--- main.cpp
+++ main.cpp
@@ -9,92 +9,92 @@ void test_intersection(Segment3D seg1, Segment3D seg2, Vector3D expected){
 }
 
 int main(){
-    // отрезки, пересекающиеся в (2,0,0)
+    // segments, intersecting in (2,0,0)
     test_intersection(Segment3D(Vector3D(2,2,0), Vector3D(2,-2,0)),
         Segment3D(Vector3D(2,0,2), Vector3D(2,0,-2)),
         Vector3D(2,0,0));
     
-    // отрезки, пересекающиеся в (0,2,0)
+    // segments, intersecting in (0,2,0)
     test_intersection(Segment3D(Vector3D(2,2,0), Vector3D(-2,2,0)),
         Segment3D(Vector3D(0,2,2), Vector3D(0,2,-2)),
         Vector3D(0,2,0));
     
-    // отрезки, пересекающиеся в (0,0,2)
+    // segments, intersecting in (0,0,2)
     test_intersection(Segment3D(Vector3D(0,2,2), Vector3D(0,-2,2)),
         Segment3D(Vector3D(2,0,2), Vector3D(-2,0,2)),
         Vector3D(0,0,2));
     
-    // отрезки, пересекающиеся в (-2,-4,2)
+    // segments, intersecting in (-2,-4,2)
     test_intersection(Segment3D(Vector3D(-2,-4,0), Vector3D(-2,-4,5)),
         Segment3D(Vector3D(0,-4,1), Vector3D(-4,-4,3)),
         Vector3D(-2,-4,2));
     
-    // отрезки, пересекающиеся в (2,3,1)
+    // segments, intersecting in (2,3,1)
     test_intersection(Segment3D(Vector3D(0,1,-1), Vector3D(3,4,2)),
         Segment3D(Vector3D(4,4,1), Vector3D(0,2,1)),
         Vector3D(2,3,1));
     
-    // непересекающиеся прямые
+    // non-intersecting segments
     test_intersection(Segment3D(Vector3D(1,2,3), Vector3D(1,1,1)),
         Segment3D(Vector3D(2,3,4), Vector3D(2,2,2)),
         Vector3D(INFINITY, INFINITY, INFINITY));
     
-    // непересекающиеся отрезки, лежащие на пересекающихся прямых
+    // non-intersecting segments, laying on intersecting lines
     test_intersection(Segment3D(Vector3D(0,1,-1), Vector3D(3,4,2)),
         Segment3D(Vector3D(6,5,1), Vector3D(4,4,1)),
         Vector3D(INFINITY, INFINITY, INFINITY));
     
-    // отрезок и не лежащая на нем точка
+    // segment and dot
     test_intersection(Segment3D(Vector3D(1,2,3), Vector3D(4,3,2)),
         Segment3D(Vector3D(3,2,1), Vector3D(3,2,1)),
         Vector3D(INFINITY, INFINITY, INFINITY));
 
-    // отрезок и лежащая на нем точка
+    // segment and dot
     test_intersection(Segment3D(Vector3D(1,2,3), Vector3D(4,3,2)),
         Segment3D(Vector3D(2.5,2.5,2.5), Vector3D(2.5,2.5,2.5)),
         Vector3D(2.5,2.5,2.5));
 
-    // несовпадающие точки
+    // different dots
     test_intersection(Segment3D(Vector3D(1,2,3), Vector3D(1,2,3)),
         Segment3D(Vector3D(4,3,7), Vector3D(4,3,7)),
         Vector3D(INFINITY, INFINITY, INFINITY));
 
-    // совпадающие точки (1,2,3)
+    // same dots (1,2,3)
     test_intersection(Segment3D(Vector3D(1,2,3), Vector3D(1,2,3)),
         Segment3D(Vector3D(1,2,3), Vector3D(1,2,3)),
         Vector3D(1,2,3));
 
-    // подотрезок, возвращаем (2.5,2.5,2.5)
+    // subsegment (2.5,2.5,2.5)
     test_intersection(Segment3D(Vector3D(1,2,3), Vector3D(4,3,2)),
         Segment3D(Vector3D(2.5,2.5,2.5), Vector3D(3.25,2.75,2.25)),
         Vector3D(2.5,2.5,2.5));
 
-    // подотрезок
+    // subsegment
     test_intersection(Segment3D(Vector3D(1,2,3), Vector3D(4,3,2)),
         Segment3D(Vector3D(2.5,2.5,2.5), Vector3D(3.25,2.75,2.25)),
         Vector3D(2.5,2.5,2.5));
 
-    // подотрезок
+    // subsegment
     test_intersection(Segment3D(Vector3D(0,0,0), Vector3D(5,5,5)),
         Segment3D(Vector3D(1,1,1), Vector3D(3,3,3)),
         Vector3D(1,1,1));
 
-    // пересекаются по отрезку
+    // have same subsegment
     test_intersection(Segment3D(Vector3D(1,2,3), Vector3D(3.25,2.75,2.25)),
         Segment3D(Vector3D(4,3,2), Vector3D(2.5,2.5,2.5)),
         Vector3D(3.25,2.75,2.25));
 
-    // пересекаются по отрезку
+    // have same subsegment
     test_intersection(Segment3D(Vector3D(0,0,0), Vector3D(3,3,3)),
         Segment3D(Vector3D(1,1,1), Vector3D(5,5,5)),
         Vector3D(3,3,3));
 
-    // коллинеарны, но не пересекаются
+    // on the same line, but have no intersection
     test_intersection(Segment3D(Vector3D(0,0,0), Vector3D(1,1,1)),
         Segment3D(Vector3D(3,3,3), Vector3D(5,5,5)),
         Vector3D(INFINITY,INFINITY,INFINITY));
 
-    // отрезок и точка, лежащая на его продолжении
+    // segment and dot, laying on the segment continuation
     test_intersection(Segment3D(Vector3D(0,0,0), Vector3D(3,3,3)),
         Segment3D(Vector3D(5,5,5), Vector3D(5,5,5)),
         Vector3D(INFINITY,INFINITY,INFINITY));
